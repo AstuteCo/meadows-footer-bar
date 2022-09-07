200c3b0d4cc6b1c9c46085e7aea6bbb0cbb8a269
Rob Petrin <robpetrin@gmail.com>
2022-09-07 14:49:03 -0400

    JS Media Query

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,18 +1,17 @@
 if (document.querySelector('#footer-live-chat')) {
     let chatToggle = document.querySelector('#footer-live-chat')
     chatToggle.addEventListener('click',function(){
-        // if (window.innerWidth > 703) {
-        //     chatToggle.classList.toggle('chat-closed')
-        // }
-        let chatWindow = document.querySelector('#divchatmain')
-        let chatWindowVisible = chatWindow.style.display
-        if (chatWindowVisible === 'none') {
-            console.log('chat closed')
-            chatToggle.classList.add('chat-closed')
-        } else {
-            console.log('chat opened')
-            chatToggle.classList.remove('chat-closed')
+        if (window.innerWidth > 703) {
+            let chatWindow = document.querySelector('#divchatmain')
+            let chatWindowVisible = chatWindow.style.display
+            if (chatWindowVisible === 'none') {
+                console.log('chat closed')
+                chatToggle.classList.add('chat-closed')
+            } else {
+                console.log('chat opened')
+                chatToggle.classList.remove('chat-closed')
+            }
+            console.log(chatToggle.classList)
         }
-        console.log(chatToggle.classList)
     })
 }
