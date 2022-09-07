d651e70b660b3e9378e4e9d478a4852b04fe597d
Rob Petrin <robpetrin@gmail.com>
2022-09-07 11:50:30 -0400

    more consoles

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -4,9 +4,11 @@ if (document.querySelector('#footer-live-chat')) {
         let chatWindow = document.querySelector('#divchatmain iframe')
         console.log(chatWindow.ariaHidden)
         if (!chatWindow.ariaHidden) {
+            console.log('chat opened')
             let chatToggle = document.querySelector('#footer-live-chat')
             chatToggle.classList.remove('chat-closed')
         } else {
+            console.log('chat closed')
             let chatToggle = document.querySelector('#footer-live-chat')
             chatToggle.classList.add('chat-closed')
         }
