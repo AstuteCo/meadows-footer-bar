9b44334ce6e6f8058922b7efc8f10caec3f9b662
Rob Petrin <robpetrin@gmail.com>
2022-09-07 12:41:30 -0400

    Revert conditional

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,20 +1,20 @@
 if (document.querySelector('#footer-live-chat')) {
     let chatToggle = document.querySelector('#footer-live-chat')
     chatToggle.addEventListener('click',function(){
-        // if (window.innerWidth > 703) {
-        //     chatToggle.classList.toggle('chat-closed')
-        // }
-        let chatWindow = document.querySelector('#divchatmain iframe')
-        console.log(chatWindow.ariaHidden)
-        if (chatWindow.ariaHidden) {
-            console.log('chat closed')
-            let chatToggle = document.querySelector('#footer-live-chat')
-            chatToggle.classList.add('chat-closed')
-        } else {
-            console.log('chat opened')
-            let chatToggle = document.querySelector('#footer-live-chat')
-            chatToggle.classList.remove('chat-closed')
+        if (window.innerWidth > 703) {
+            chatToggle.classList.toggle('chat-closed')
         }
-        console.log(chatToggle.classList)
+        // let chatWindow = document.querySelector('#divchatmain iframe')
+        // console.log(chatWindow.ariaHidden)
+        // if (!chatWindow.ariaHidden) {
+        //     console.log('chat opened')
+        //     let chatToggle = document.querySelector('#footer-live-chat')
+        //     chatToggle.classList.remove('chat-closed')
+        // } else {
+        //     console.log('chat closed')
+        //     let chatToggle = document.querySelector('#footer-live-chat')
+        //     chatToggle.classList.add('chat-closed')
+        // }
+        // console.log(chatToggle.classList)
     })
 }
