8590c370faf49b289760533b14a41c8005ff79ba
Rob Petrin <robpetrin@gmail.com>
2022-09-07 11:31:56 -0400

    Different measurement metric

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,9 +1,9 @@
 if (document.querySelector('#divchatmain')) {
-    let chatWindow = document.querySelector('#divchatmain')
+    let chatWindow = document.querySelector('#divchatmain iframe')
     let chatToggle = document.querySelector('#footer-live-chat')
     
     chatToggle.addEventListener('click',function(){
-        console.log(chatWindow.style.display)
+        console.log(!chatWindow.ariaHidden)
         if (chatWindow.style.display == 'block') {
             chatToggle.classList.remove('chat-closed')
         } else {
