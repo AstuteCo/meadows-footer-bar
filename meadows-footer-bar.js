ce216975fe17040e03baf188131402642aa2b953
Rob Petrin <robpetrin@gmail.com>
2022-09-07 11:42:41 -0400

    more consoles again

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,7 +1,9 @@
+console.log(document.querySelector('#divchatmain iframe'))
 if (document.querySelector('#divchatmain iframe')) {
     let chatWindow = document.querySelector('#divchatmain iframe')
     let chatToggle = document.querySelector('#footer-live-chat')
 
+    console.log(chatWindow)
 
     chatToggle.addEventListener('click',function(){
         console.log(chatWindow.ariaHidden)
