ea2944fbfdf51f73ea8d9bffd29213c1d4b1a467
Rob Petrin <robpetrin@gmail.com>
2022-09-07 11:33:14 -0400

    Fix metric

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -3,8 +3,7 @@ if (document.querySelector('#divchatmain')) {
     let chatToggle = document.querySelector('#footer-live-chat')
     
     chatToggle.addEventListener('click',function(){
-        console.log(!chatWindow.ariaHidden)
-        if (chatWindow.style.display == 'block') {
+        if (!chatWindow.ariaHidden) {
             chatToggle.classList.remove('chat-closed')
         } else {
             chatToggle.classList.add('chat-closed')
