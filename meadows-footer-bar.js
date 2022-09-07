8eeb4cf9e4e01cb4ae1b6ffb3b14c8ad73d9fd56
Rob Petrin <robpetrin@gmail.com>
2022-09-07 11:46:16 -0400

    Adjusted variable declarations

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,11 +1,12 @@
 if (document.querySelector('#footer-live-chat')) {
-    let chatToggle = document.querySelector('#footer-live-chat')
     chatToggle.addEventListener('click',function(){
         let chatWindow = document.querySelector('#divchatmain iframe')
         console.log(chatWindow.ariaHidden)
         if (!chatWindow.ariaHidden) {
+            let chatToggle = document.querySelector('#footer-live-chat')
             chatToggle.classList.remove('chat-closed')
         } else {
+            let chatToggle = document.querySelector('#footer-live-chat')
             chatToggle.classList.add('chat-closed')
         }
         console.log(chatToggle.classList)
