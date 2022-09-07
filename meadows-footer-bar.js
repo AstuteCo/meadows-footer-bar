8c0810ccd1a21675cb0ad88ba9d7d9c632bc5a2c
Rob Petrin <robpetrin@gmail.com>
2022-09-07 12:03:16 -0400

    Static Toggle

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,17 +1,18 @@
 if (document.querySelector('#footer-live-chat')) {
     let chatToggle = document.querySelector('#footer-live-chat')
     chatToggle.addEventListener('click',function(){
-        let chatWindow = document.querySelector('#divchatmain iframe')
-        console.log(chatWindow.ariaHidden)
-        if (!chatWindow.ariaHidden) {
-            console.log('chat opened')
-            let chatToggle = document.querySelector('#footer-live-chat')
-            chatToggle.classList.remove('chat-closed')
-        } else {
-            console.log('chat closed')
-            let chatToggle = document.querySelector('#footer-live-chat')
-            chatToggle.classList.add('chat-closed')
-        }
-        console.log(chatToggle.classList)
+        chatToggle.classList.toggle('chat-closed')
+        // let chatWindow = document.querySelector('#divchatmain iframe')
+        // console.log(chatWindow.ariaHidden)
+        // if (!chatWindow.ariaHidden) {
+        //     console.log('chat opened')
+        //     let chatToggle = document.querySelector('#footer-live-chat')
+        //     chatToggle.classList.remove('chat-closed')
+        // } else {
+        //     console.log('chat closed')
+        //     let chatToggle = document.querySelector('#footer-live-chat')
+        //     chatToggle.classList.add('chat-closed')
+        // }
+        // console.log(chatToggle.classList)
     })
 }
