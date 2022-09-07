dc340e1b43778ddb06204b6c2f0023675092c2ef
Rob Petrin <robpetrin@gmail.com>
2022-09-07 14:40:43 -0400

    Different conditional

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -1,20 +1,18 @@
 if (document.querySelector('#footer-live-chat')) {
     let chatToggle = document.querySelector('#footer-live-chat')
     chatToggle.addEventListener('click',function(){
-        if (window.innerWidth > 703) {
-            chatToggle.classList.toggle('chat-closed')
-        }
-        // let chatWindow = document.querySelector('#divchatmain iframe')
-        // console.log(chatWindow.ariaHidden)
-        // if (!chatWindow.ariaHidden) {
-        //     console.log('chat opened')
-        //     let chatToggle = document.querySelector('#footer-live-chat')
-        //     chatToggle.classList.remove('chat-closed')
-        // } else {
-        //     console.log('chat closed')
-        //     let chatToggle = document.querySelector('#footer-live-chat')
-        //     chatToggle.classList.add('chat-closed')
+        // if (window.innerWidth > 703) {
+        //     chatToggle.classList.toggle('chat-closed')
         // }
-        // console.log(chatToggle.classList)
+        let chatWindow = document.querySelector('#divchatmain')
+        let chatWindowVisible = chatWindow.style.display
+        if (chatWindowVisible === 'none') {
+            console.log('chat closed')
+            chatToggle.classList.add('chat-closed')
+        } else {
+            console.log('chat opened')
+            chatToggle.classList.remove('chat-closed')
+        }
+        console.log(chatToggle.classList)
     })
 }
