f1e8bcba4b41d16a8d8aa510d4647a4b55d96699
Rob Petrin <robpetrin@gmail.com>
2022-09-07 11:28:32 -0400

    Less strict comparison

--- meadows-footer-bar.js
+++ meadows-footer-bar.js
@@ -3,7 +3,8 @@ if (document.querySelector('#divchatmain')) {
     let chatToggle = document.querySelector('#footer-live-chat')
     
     chatToggle.addEventListener('click',function(){
-        if (chatWindow.style.display === 'block') {
+        console.log(chatWindow.style.display)
+        if (chatWindow.style.display == 'block') {
             chatToggle.classList.remove('chat-closed')
         } else {
             chatToggle.classList.add('chat-closed')
