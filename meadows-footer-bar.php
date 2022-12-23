af41194289cf351e05e4c558f9537c49bffc0089
Rob Petrin <robpetrin@gmail.com>
2022-12-23 10:56:42 -0500

    Version Bump to Test Remote Updater

--- meadows-footer-bar.php
+++ meadows-footer-bar.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:   Meadows Footer Bar
  * Description:   Contact options that appear as a bar at the bottom of pages.
- * Version:           1.27
+ * Version:           1.28
  * Requires at least: 5.9.3
  * Requires PHP:  7.4
  * Author:            Astute Communications
